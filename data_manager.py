e6acde8fbaeb6dc09cc63b84b656fbc5c88a6193
clssadik <cillsadik@gmail.com>
2026-06-05 22:25:36 +0300

    Implement DataManager with Sheety API

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import requests
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    
+    def __init__(self):
+        pass
+    
+    def get_data(self):
+        SHEETY_AUTH = os.environ["SHEETY_AUTH"]
+        SHEETY_URL = "https://api.sheety.co/0473906867a46fc610805d81714a83fa/flightDeals/prices"
+
+        sheets_headers = {
+            "Authorization" : SHEETY_AUTH,
+        }
+
+        response = requests.get(url=SHEETY_URL,headers=sheets_headers)
+        data = response.json()
+        return data["prices"]
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-import requests
+from data_manager import DataManager
 
 SERP_API = os.environ["SERP_API"]
-SHEETY_AUTH = os.environ["SHEETY_AUTH"]
-SHEETY_URL = "https://api.sheety.co/0473906867a46fc610805d81714a83fa/flightDeals/prices"
+
+data_manager = DataManager()
+sheets_data = data_manager.get_data()
+print(sheets_data)
