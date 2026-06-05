2f680fac2c95df61781097f72dd335d786431634
clssadik <cillsadik@gmail.com>
2026-06-05 22:03:51 +0300

    Add requests import and Sheety URL

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import requests
 
 SERP_API = os.environ["SERP_API"]
 SHEETY_AUTH = os.environ["SHEETY_AUTH"]
+SHEETY_URL = "https://api.sheety.co/0473906867a46fc610805d81714a83fa/flightDeals/prices"
