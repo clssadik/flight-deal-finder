d766db5166703b3572679c102f6e12f06ccb7bd2
clssadik <cillsadik@gmail.com>
2026-06-07 15:58:21 +0300

    Update flight search params

--- flight_search.py
+++ flight_search.py
@@ -1,6 +1,8 @@
 from main import load_dotenv,os
 load_dotenv()
 import serpapi
+import requests_cache
+
 
 class FlightSearch:
 
@@ -8,13 +10,18 @@ class FlightSearch:
         self.API = os.environ["SERP_API"]
 
     def get_search_data(self):
+        requests_cache.install_cache('flight_cache', expire_after=3600)
+
         client = serpapi.Client(api_key=self.API)
         results = client.search({
-            "engine": "google_flights",
-            "departure_id": "CDG",
-            "arrival_id": "AUS",
-            "currency": "USD",
-            "type": "2",
-            "outbound_date": "2026-06-07"
+            for i in range(1,3):{
+                
+            }
+                "engine": "google_flights",
+                "departure_id": "COV",
+                "arrival_id": "AUS",
+                "currency": "USD",
+                "type": "2",
+                "outbound_date": "2026-06-07"
         })
         best_flights = results["best_flights"]
--- main.py
+++ main.py
@@ -13,4 +13,5 @@ six_month_from_today = datetime.now() + timedelta(days=182)
 
 data_manager = DataManager()
 sheets_data = data_manager.get_google_sheets_data()
-pprint(sheets_data)
+
+# pprint(sheets_data)
