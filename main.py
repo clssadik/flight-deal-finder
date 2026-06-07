e268ba7e03c397442439892b7659f7c4b0f6fd6c
clssadik <cillsadik@gmail.com>
2026-06-07 15:00:27 +0300

    Add pprint for sheet data

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from data_manager import DataManager
+from pprint import pprint
+import requests_cache
 
 SERP_API = os.environ["SERP_API"]
 
 data_manager = DataManager()
 sheets_data = data_manager.get_data()
-print(sheets_data)
+pprint(sheets_data)
